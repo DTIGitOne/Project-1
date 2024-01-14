cdfb9d87f5f9c886737ec6511e74fe951276ce86
DTIGitOne <dammirtaljanovic@gmail.com>
2024-01-14 03:41:05 +0100

    feat: 90% finished signup

--- SignUpPage/signup.js
+++ SignUpPage/signup.js
@@ -3,10 +3,11 @@ let userNameError = document.getElementById("userNameError");
 
 let signup = document.getElementById("signup");
 
-let userNameRegex = /^[a-z]{1}[a-z]{1,25}$/
-let emailRegex = /^[a-zA-Z0-9._]+@[a-zA-Z]+\.?[a-zA-z]+?\.[a-zA-Z]{2,}$/;
-let passwordRegex = /[a-zA-Z0-9!@#$%^&*()-_=+{}|[\];:'",.<>?\\]{6,30}/;
-let birthdateRegex = /^[1-2][9,0][0-9][0-9]-[0-1][0-9]-[0-9]{2}$/;
+let userNameRegex = /^[a-zA-z0-9_.-]{1}[a-zA-z0-9_.-]{1,25}$/
+let emailRegex = /^[a-zA-Z0-9._]+@[a-zA-Z]+\.?[a-zA-z]+?\.[a-zA-Z]{1,}$/;
+let passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\];:'",.<>?\\d]).{6,30}$/;
+let birthdateRegex = /^[1-2][9,8,0][0-9][0-9]-[0-1][0-9]-[0-9]{2}$/;
+let specialRegex = /[!@#$%^&*()_+{}\[\];:'",.<>?\\]/;
 
 signup.addEventListener('mouseover' , function () {
    signup.style.backgroundColor = "#C9BAFF";
@@ -27,9 +28,30 @@ let birthdateError = document.getElementById("birthdateError");
 
 let showPasswordBox = document.getElementById("showPasswordBox");
 let showPassword = document.getElementById("showPassword");
+let showPasswordBox2 = document.getElementById("showPasswordBox2");
+let showPassword2 = document.getElementById("showPassword2");
+let psswordError = document.getElementById ("passwordError");
 
+userName.addEventListener('input' , function() {
+
+   userNameError.className = "errorBox1";
+   userNameError.innerHTML = "";
+
+   if (userName.value.length === 25) {
+      userNameError.innerHTML = "username can only be 25 characters long";
+   } else if (userName.value.length < 25) {
+      userNameError.innerHTML = "";
+   }
+   
+});
 
 password.addEventListener("input" , function() {
+   
+   if (password.value.length === 30) {
+      passwordError.innerHTML = "Password can only be 30 characters long";
+   } else if (password.value.length < 30) {
+      passwordError.innerHTML = "";
+   }
    
 
    if (password.value !== "") {
@@ -37,8 +59,7 @@ password.addEventListener("input" , function() {
    } else if (password.value === "") {
       showPassword.className = "checkbox";
    }
-      
-   
+ 
 
    showPassword.addEventListener('mouseover', function() {
       showPasswordBox.style.display = "inline";
@@ -48,11 +69,57 @@ password.addEventListener("input" , function() {
       showPasswordBox.style.display = "none";
    }
    );
+
 }
 );
 
+showPassword.addEventListener('click' , function(){
+   if (showPassword.checked === true) {
+      password.type = "text";
+   } else if (showPassword.checked === false) {
+      password.type = "password";
+   }
+}
+);
+
+
+repeatPassword.addEventListener("input" , function() {
+   
+
+   if (repeatPassword.value !== "") {
+      showPassword2.className = "checkbox2";
+   } else if (repeatPassword.value === "") {
+      showPassword2.className = "checkbox";
+   }
+      
+   repeatPasswordError.innerHTML = "";
+
+   showPassword2.addEventListener('mouseover', function() {
+      showPasswordBox2.style.display = "inline";
+   }
+   );
+   showPassword2.addEventListener('mouseleave', function() {
+      showPasswordBox2.style.display = "none";
+   }
+   );
+}
+);
+
+showPassword2.addEventListener('click' , function(){
+   if (showPassword2.checked === true) {
+      repeatPassword.type = "text";
+   } else if (showPassword2.checked === false) {
+      repeatPassword.type = "password";
+   }
+}
+);
+
+
 signup.addEventListener('click' , function() {
-        
+   
+   userNameError.className = "errorBox1";
+   userNameError.innerHTML = "";
+
    if ( userName.value , email.value , password.value , repeatPassword.value , birthdate.value === "") {
 
       signupError.innerHTML = "Please fill in all the data";
@@ -60,23 +127,28 @@ signup.addEventListener('click' , function() {
       userName.addEventListener('input' , function(){
          signupError.innerHTML = "";
          userNameError.innerHTML = "";
+         repeatPasswordError.innerHTML = "";
       });
       email.addEventListener('input' , function(){
          signupError.innerHTML = "";
          userNameError.innerHTML = "";
+         repeatPasswordError.innerHTML = "";
       });
       password.addEventListener('input' , function(){
          signupError.innerHTML = "";
          userNameError.innerHTML = "";
+         repeatPasswordError.innerHTML = "";
       });
       repeatPassword.addEventListener('input' , function(){
          signupError.innerHTML = "";
          userNameError.innerHTML = "";
+         repeatPasswordError.innerHTML = "";
       });
       birthdate.addEventListener('input' , function(){
          signupError.innerHTML = "";
          userNameError.innerHTML = "";
          birthdateError.innerHTML = "";
+         repeatPasswordError.innerHTML = "";
       });
 
    } else {
@@ -89,7 +161,7 @@ signup.addEventListener('click' , function() {
            
             emailError.innerHTML = "";
             
-            if (password.value.match(passwordRegex) && password.value.length <= 30) {
+            if (password.value.match(passwordRegex) && password.value.match(specialRegex)) {
                
                passwordError.innerHTML = "";
 
@@ -99,20 +171,31 @@ signup.addEventListener('click' , function() {
                   
 
                   if (birthdate.value.match(birthdateRegex)) {
-
+                     
+                     
                      let year1 = birthdate.value.slice(0,4);
 
                      let year1Number = parseInt(year1);
-
-                     if (year1Number >= 1930) {
-                        birthdateError.innerHTML = "";
-                        
-                        let dateStringRemove = birthdate.value.replace(/-/g, '');
-                        let dateFull = parseInt(dateStringRemove);
-                        
-
+                     
+                     if (year1Number > 2004) {
+                        birthdateError.innerHTML = "please select a legal age";
                      } else {
-                        birthdateError.innerHTML = "the minimum age is 1930";
+                        if (year1Number >= 1930) {
+                           birthdateError.innerHTML = "";
+                           
+                           let dateStringRemove = birthdate.value.replace(/-/g, '');
+                           let dateFull = parseInt(dateStringRemove);
+                           
+                           let info = {
+                              name : userName.value,
+                              mail : email.value,
+                              password1 : password.value,
+                              date1 : dateFull,
+                           };
+   
+                        } else {
+                           birthdateError.innerHTML = "the minimum age is 1930";
+                        }
                      }
 
                   } else {
@@ -128,13 +211,17 @@ signup.addEventListener('click' , function() {
                }
 
             } else {
-               
-               let psswordError = document.getElementById ("passwordError");
-
-               if (password.value.length > 30 ) {
-                  passwordError.innerHTML = "password can be 30 characters long";
-               } else if (password.value.length < 6) {
+               console.log("no");
+               if (password.value.length < 6 ) {
                   passwordError.innerHTML = "password must at least contain 6 characters";
+               } 
+
+               if (!password.value.match(passwordRegex)) {
+                  passwordError.innerHTML = "please add one special and uppercase character";
+               }
+               
+               if (!password.value.match(specialRegex)) {
+                  passwordError.innerHTML = "please add one special and uppercase character";
                }
             }
 
@@ -170,12 +257,6 @@ signup.addEventListener('click' , function() {
 
 
 
-   let info = {
-      name : userName.value,
-      mail : email.value,
-      password1 : password,
-      password2 : repeatPassword,
-      date1 : birthdate,
-   };
+   
    
 });
